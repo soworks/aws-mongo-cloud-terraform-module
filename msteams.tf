495f5ad027e75c09c7a21009bb6a42e5e030e28a
Alberto Solano <146444006+soworks@users.noreply.github.com>
2024-05-21 12:29:37 -0400

    Update msteams.tf

--- msteams.tf
+++ msteams.tf
@@ -1,6 +1,6 @@
 module "msteams_notification" {
   #checkov:skip=CKV_TF_1: "Ensure Terraform module sources use a commit hash - this is annoying for private modules"
-  source = "git::ssh://git@bitbucket.org/lexipol/terraform-aws-teams-notifications.git?ref=v1.0.4"
+  source = "./../"
 
   name           = "${local.cluster_name}-notifications"
   sns_topic_name = "${local.cluster_name}-notifications"
